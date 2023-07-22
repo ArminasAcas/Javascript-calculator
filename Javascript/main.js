b148a7e2c53716c3a1d60bc5089deb2f9f0689cc
Spectronas <arminas.acas@sa.stud.vu.lt>
2023-07-22 17:51:00 +0300

    Added calculations for operations + - / * √

--- Javascript/main.js
+++ Javascript/main.js
@@ -2,43 +2,113 @@ document.addEventListener('DOMContentLoaded', function () {
     class Calculator {
 
         constructor(){
-            this.equation = "";
+            this.displayText = "";
             this.numbers = [];
             this.operations = [];
             this.on = true;
+            this.operationCharacters = ["-","+","/","*", "!", "√"];
         }
 
         clearScreen()
         {
-            this.equation = "";        
+            this.displayText = "";
+            this.numbers = []; 
+            this.operations = [];       
         }
 
         addNumber(number)
         {
             if (this.on)
             {
-                this.equation += number;
+                if (this.numbers.length <= 0)
+                {
+                    this.numbers.push(number);
+                    this.displayText += number;
+                    return;
+                }
+                
+                if (this.operationCharacters.includes(this.displayText[this.displayText.length-1]))
+                {
+                    this.numbers.push(number);
+                    this.displayText += number;
+                    return;
+                }
+                
+                this.numbers[this.numbers.length-1] += number;
+                this.displayText += number;
             }
-            
+
         }
 
         addOperation(operation)
         {
             if (this.on)
             {
-                this.equation += operation;
+                this.displayText += operation;
+                this.operations.push(operation);
             }
             
         }
 
         displayScreen()
         {
-            displayText.innerHTML = this.equation;
+            displayText.innerHTML = this.displayText;
         }
 
-        displayResult()
+        calculateResult()
         {
+            this.numbers = this.numbers.map(Number);
+        
+            let i = 0;
+            while (i <= this.operations.length)
+            {
+                if (this.operations[i] === "√") 
+                {
+                    this.numbers[i] = Math.sqrt(this.numbers[i]);
+                    this.operations.splice(i,1);
+                }
+                else i++;
+            };
 
+            i = 0;
+            while (i <= this.operations.length)
+            {
+                if (this.operations[i] === "*") 
+                {
+                    this.numbers[i] = this.numbers[i] * this.numbers[i+1];
+                    this.numbers.splice(i+1,1);
+                    this.operations.splice(i,1);
+                }
+                else if (this.operations[i] === "/") 
+                {
+                    this.numbers[i] = this.numbers[i] / this.numbers[i+1];
+                    this.numbers.splice(i+1,1);
+                    this.operations.splice(i,1);
+                }
+                else i++;
+            };
+
+            i = 0;
+            while (i <= this.operations.length)
+            {
+                if (this.operations[i] === "+") 
+                {
+                    this.numbers[i] = this.numbers[i] + this.numbers[i+1];
+                    this.numbers.splice(i+1,1);
+                    this.operations.splice(i,1);
+                }
+                else if (this.operations[i] === "-") 
+                {
+                    this.numbers[i] = this.numbers[i] - this.numbers[i+1];
+                    this.numbers.splice(i+1,1);
+                    this.operations.splice(i,1);
+                }
+                else i++;
+            };
+        
+
+            if (!Number.isInteger(this.numbers[0])) this.displayText = String(this.numbers[0].toFixed(2));
+            else this.displayText = String(this.numbers[0]);
         }
 
         changeDeviceState()
@@ -97,6 +167,15 @@ document.addEventListener('DOMContentLoaded', function () {
         onButton.addEventListener('click', () => 
         {
             calculator.changeDeviceState();
+        })
+    }
+
+    if (equalButton)
+    {
+        equalButton.addEventListener('click', () => 
+        {
+            calculator.calculateResult();
+            calculator.displayScreen();
         })
     }
 
